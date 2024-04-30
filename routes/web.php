7af79e2ecdf7a3dd6faa254c97d074b7890eaef9
AndyVodovozov <andy_vodovozov@mail.ru>
2024-04-30 20:56:48 +0500

    Второй

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,23 @@
 <?php
 
+use App\Http\Controllers\TestController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/test', [TestController::class, 'index']);
+
+//Route::redirect('/user', '/owner');
+Route::view('/info', 'welcome', ['name' => 'Andy']);
+
+Route::post('/info1', [TestController::class, 'index']);
+Route::put('/info2', [TestController::class, 'index']);
+Route::patch('/info3', [TestController::class, 'index']);
+Route::delete('/info4', [TestController::class, 'index']);
+Route::options('/info5', [TestController::class, 'index']);
+
+//Route::match(['get', 'post'], '/info', function (Request:) {
+//    dd('match');
+//});
